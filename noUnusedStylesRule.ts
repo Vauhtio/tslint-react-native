d241c1bad2906dcd3a9a0491be7ecebd9cd89ab3
Henrik Raitasola <henrik.raitasola@gmail.com>
2019-01-01 19:39:15 +0200

    Add initial version of unused styles rule

---
+++ noUnusedStylesRule.ts
@@ -0,0 +1,68 @@
+import * as ts from 'typescript';
+import * as Lint from 'tslint';
+
+export class Rule extends Lint.Rules.AbstractRule {
+  public static FAILURE_STRING = 'This style is not used';
+
+  public apply(sourceFile: ts.SourceFile): Lint.RuleFailure[] {
+    return this.applyWithWalker(new NoImportsWalker(sourceFile, this.getOptions()));
+  }
+}
+
+class NoImportsWalker extends Lint.RuleWalker {
+  private stylesheets: Record<string, ts.NodeArray<ts.ObjectLiteralElementLike>> = {};
+  private usedProperties: string[] = [];
+  public visitVariableDeclaration(node: ts.VariableDeclaration) {
+    if (this.isStyleSheetNode(node)) {
+      console.log(node.name.getText());
+      if (node.initializer) {
+        node.initializer.forEachChild(child => {
+          if (ts.isObjectLiteralExpression(child)) {
+            this.stylesheets[node.name.getText()] = child.properties;
+          }
+        });
+      }
+    }
+    super.visitVariableDeclaration(node);
+  }
+
+  public visitPropertyAccessExpression(node: ts.PropertyAccessExpression) {
+    if (node.expression.getText() === 'styles') {
+      this.usedProperties.push(node.name.getText());
+    }
+
+    super.visitPropertyAccessExpression(node);
+  }
+
+  public visitEndOfFileToken(node: ts.EndOfFileToken) {
+    Object.values(this.stylesheets).forEach(stylesheet => {
+      stylesheet.forEach(child => {
+        if (child.name) {
+          if (!this.usedProperties.includes(child.name.getText())) {
+            this.addFailure(
+              this.createFailure(child.getStart(), child.getWidth(), Rule.FAILURE_STRING),
+            );
+          }
+        }
+      });
+    });
+    super.visitEndOfFileToken(node);
+  }
+
+  private isStyleSheetNode(node: ts.VariableDeclaration) {
+    const stylesheet =
+      node.initializer &&
+      node.initializer.getChildAt(0) &&
+      node.initializer.getChildAt(0).getChildAt(0);
+    const createCall =
+      node.initializer &&
+      node.initializer.getChildAt(0) &&
+      node.initializer.getChildAt(0).getChildAt(2);
+    return (
+      stylesheet &&
+      stylesheet.getText() === 'StyleSheet' &&
+      createCall &&
+      createCall.getText() === 'create'
+    );
+  }
+}
